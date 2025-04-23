faf9a601c486e789b49f53b76c2d7953ca8740fb
Yasin Cüre <yasindenizcure@gmail.com>
2025-04-23 18:38:51 +0300

    Admin Panel & Layout

---
+++ ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents.LayoutViewComponents
+{
+    public class _LayoutNavbarComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    
+}
+
