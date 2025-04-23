d9aeebfb67942df8f178b96e25ef38c5b5f6679a
Yasin Cüre <yasindenizcure@gmail.com>
2025-04-23 19:20:21 +0300

    Deneyim CRUD İşlemleri

--- Controllers/ExperienceController.cs
+++ Controllers/ExperienceController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
 
 namespace MyPortfolio.Controllers
 {
     public class ExperienceController : Controller
     {
+        MyPortfolioContext _context = new MyPortfolioContext();
         public IActionResult ExperienceList()
         {
+            var values = _context.Experiences.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            _context.Experiences.Add(experience);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+            _context.Experiences.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            _context.Experiences.Update(experience);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
