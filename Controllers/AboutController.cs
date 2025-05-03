a7e1f5a57db529df3ef676385cdfc87a3d04cfbe
Yasin Cüre <yasindenizcure@gmail.com>
2025-05-03 22:31:14 +0300

    About, Testimonial, SocialMedia & Default Index

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult AboutList()
+        {
+            var values = context.Abouts.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateAbout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateAbout(About about)
+        {
+            context.Abouts.Add(about);
+            context.SaveChanges();
+            return RedirectToAction("AboutList");
+        }
+        public IActionResult DeleteAbout(int id)
+        {
+            var value = context.Abouts.Find(id);
+            context.Abouts.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("AboutList");
+        }
+        [HttpGet]
+        public IActionResult UpdateAbout(int id)
+        {
+            var value = context.Abouts.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateAbout(About about)
+        {
+            context.Abouts.Update(about);
+            context.SaveChanges();
+            return RedirectToAction("AboutList");
+        }
+    }
+}
---
+++ Controllers/SocialMediaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult SocialMediaList()
+        {
+            var values = context.SocialMedias.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateSocialMedia(SocialMedia socialMedia)
+        {
+            context.SocialMedias.Add(socialMedia);
+            context.SaveChanges();
+            return RedirectToAction("SocialMediaList");
+        }
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var value = context.SocialMedias.Find(id);
+            context.SocialMedias.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SocialMediaList");
+        }
+        [HttpGet]
+        public IActionResult UpdateSocialMedia(int id)
+        {
+            var value = context.SocialMedias.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateSocialMedia(SocialMedia socialMedia)
+        {
+            context.SocialMedias.Update(socialMedia);
+            context.SaveChanges();
+            return RedirectToAction("SocialMediaList");
+        }
+    }
+}
