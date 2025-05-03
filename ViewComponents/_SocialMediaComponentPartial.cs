cd65ae969586563ba6db5b92a19c7ef3b9622b4d
Yasin Cüre <yasindenizcure@gmail.com>
2025-05-04 01:54:41 +0300

    SocialMedia

---
+++ ViewComponents/_SocialMediaComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _SocialMediaComponentPartial: ViewComponent
+    {
+        MyPortfolioContext _portfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var values = _portfolioContext.SocialMedias.ToList();
+            return View(values);
+        }
+    }
+    
+}
+
