5fcf48e0b080b808b65f6a245eae0fd08625c5e5
Yasin Cüre <yasindenizcure@gmail.com>
2025-05-04 16:19:31 +0300

    Bitti.

--- Controllers/StatisticController.cs
+++ Controllers/StatisticController.cs
@@ -10,9 +10,14 @@ namespace MyPortfolio.Controllers
         {
             ViewBag.v1 = context.Skills.Count();
             ViewBag.v2 = context.Messages.Count();
-            ViewBag.v3 = context.Messages.Where(x => x.IsRead==false).Count();
-            ViewBag.v4 = context.Messages.Where(x => x.IsRead==true).Count();
+            ViewBag.v3 = context.Messages.Where(x => x.IsRead == false).Count();
+            ViewBag.v4 = context.Messages.Where(x => x.IsRead == true).Count();
+            ViewBag.v5 = context.Testimonials.Count();
+            ViewBag.v6 = context.Experiences.Count();
+            ViewBag.v7 = context.SocialMedias.Count();
+            ViewBag.v8 = context.ToDoLists.Count();
             return View();
+
         }
     }
 }
--- ViewComponents/_StatisticComponentPartial.cs
+++ ViewComponents/_StatisticComponentPartial.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
